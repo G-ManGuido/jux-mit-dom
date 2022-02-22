f1dcac112ff96e86f3bf393e83a5f114b9a78f41
G-ManGuido <supercoder@AirvonSperCoder.fritz.box>
2022-02-22 15:58:06 +0100

    first push

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+// -----lev1_2-----
+const example = document.getElementsByClassName('example');
+
+myFunction = () => {
+  for (let i = 0; i < example.length; i++) {
+    example[i].style.backgroundColor = 'black';
+  }
+
+  example[4].style.color = 'white';
+}
+
+
+// -----lev2_3-----
+let item = document.getElementsByClassName('item');
+
+changeMe = () => {
+  item[0].style.backgroundColor = '#f6c89f';
+  item[1].style.backgroundColor = '#ffe7d1';
+  item[2].style.backgroundColor = '#4b8e8d';
+  item[3].style.backgroundColor = '#396362';
+}
+
+
+// -----lev2_4-----
+const button = document.getElementById('button');
+const changeColor = document.getElementById('farbeAuswahlen');
+
+button.addEventListener('click', function () {
+  event.preventDefault();
+  let bgColor = changeColor.value.toLowerCase().replace(' ', '');
+  document.getElementsByTagName('form')[0].style.backgroundColor = bgColor;
+});
